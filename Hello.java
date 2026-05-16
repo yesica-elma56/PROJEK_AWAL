377b498ac9792ed187c226612c360b53cea6714c
Mhd. Nabil Arkan <mhdnabilarkan@unisi.ac.id>
2026-05-16 09:53:57 +0700

    Add Prak6 class to calculate rectangle area

--- Hello.java
+++ Hello.java
@@ -1,1 +1,9 @@
-
+public class Prak6 {
+    public static void main (String[] args) {
+        float panjang = 12.5f;
+        float lebar = 5.5f;
+        float luas = panjang * lebar;
+        
+        System.out.println("Luas persegi panjang tersebut =" + luas);
+    }
+}
